88aa5b8b1f1318b0a1fa6c9862add4430c91e85b
Sandra <s.alvarez-carretero@qmul.ac.uk>
2018-03-07 18:19:57 +0000

    Removed old files

--- data-raw/canids19x29.raw.R
+++
@@ -1,7 +0,0 @@
-# Load raw data from inst/extdata
-
-canids19x29.raw <- read.table(file="inst/extdata/canids19x29.csv", sep=",")
-
-# Save raw data in rda format
-
-devtools::use_data(canids19x29.raw)
--- data-raw/vulpes21x29.raw.R
+++
@@ -1,7 +0,0 @@
-# Load raw data from inst/extdata
-
-vulpes21x29.raw <- read.table(file="inst/extdata/vulpes21x29.csv", sep=",")
-
-# Save raw data in rda format
-
-devtools::use_data(vulpes21x29.raw)
